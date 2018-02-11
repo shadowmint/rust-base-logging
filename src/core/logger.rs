f67cfe282bb4d5322cdb2ea5f38a7417b3635a24
Douglas Linder <linderd@iinet.net.au>
2018-02-11 22:14:32 +0800

    initial commit

---
+++ src/core/logger.rs
@@ -0,0 +1,39 @@
+extern crate time;
+
+use core::traits::Log;
+use core::traits::Loggable;
+use core::traits::LogFormatter;
+use core::level::Level;
+use ::formatters::DefaultFormatter;
+
+pub struct Logger {
+    loggers: Vec<Box<Log + Send>>,
+    format: Box<LogFormatter + Send>,
+}
+
+impl<'a> Logger {
+    pub fn new() -> Logger {
+        return Logger {
+            loggers: Vec::new(),
+            format: Box::new(DefaultFormatter::new()),
+        };
+    }
+
+    pub fn with<T>(mut self, logger: T) -> Logger where T: Log + Send + 'static {
+        self.loggers.push(Box::new(logger) as Box<Log + Send>);
+        return self;
+    }
+
+    pub fn with_format<T>(mut self, formatter: T) -> Logger where T: LogFormatter + Send + 'static {
+        self.format = Box::new(formatter);
+        return self;
+    }
+
+    pub fn log<'b, 'c, T>(&'c mut self, level: Level, message: T) where T: Loggable + 'b {
+        let now = time::now();
+        let raw = self.format.log_format(level, now, message.log_message(), message.log_properties());
+        for target in self.loggers.iter_mut() {
+            target.log(&raw);
+        }
+    }
+}
---
+++ src/formatters/default_formatter.rs
@@ -0,0 +1,42 @@
+extern crate time;
+
+use std::collections::HashMap;
+use ::{Level, LogFormatter};
+use self::time::Tm;
+
+pub struct DefaultFormatter {}
+
+impl DefaultFormatter {
+    pub fn new() -> DefaultFormatter {
+        return DefaultFormatter {};
+    }
+
+    fn combine(&self, message: Option<&str>, properties: Option<HashMap<&str, &str>>) -> String {
+        let mut rtn = String::new();
+        match message {
+            Some(msg) => {
+                rtn.push_str(msg);
+            }
+            None => {}
+        };
+        match properties {
+            Some(props) => {
+                for (key, value) in props.iter() {
+                    rtn.push_str(&format!(" {}:{}", key, value));
+                }
+            }
+            None => {}
+        }
+        return rtn;
+    }
+}
+
+impl LogFormatter for DefaultFormatter {
+    fn log_format(&self, level: Level, timestamp: Tm, message: Option<&str>, properties: Option<HashMap<&str, &str>>) -> String {
+        let timestring = match time::strftime("%b %d %H:%M:%S", &timestamp) {
+            Ok(i) => i,
+            Err(_) => String::from("")
+        };
+        return format!("{} {:?} - {}", timestring, level, self.combine(message, properties));
+    }
+}
